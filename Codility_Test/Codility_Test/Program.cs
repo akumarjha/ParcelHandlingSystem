c3ef97173ed55acce8531793b91f032e5f6a45c4
Ashutosh Jha <ashutosh.jha@vanderlande.com>
2019-11-24 22:32:10 +0100

    Add existing file

---
+++ Codility_Test/Codility_Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            int times = p.solution("011111");
+            Console.WriteLine("Number of time to iterate = {0},{1}", times,int.MaxValue);
+            Console.ReadLine();
+        }
+
+        public int solution(string S)
+        {
+           int count = 0;
+            try
+            {
+                int number = Convert.ToInt32(S, 2);
+                while (number != 0)
+                {
+                    count++;
+                    if (number % 2 != 0)
+                    {
+                        number = number - 1;
+                    }
+                    else
+                    {
+                        number = number / 2;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is some issue please check..{0}", ex.StackTrace);
+            }
+            return count;
+        }
+    }
+}
---
+++ ParcelHandlingSystem/ContainerProcessing/ParcelProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Models;
+using System.IO;
+using System.Data;
+
+
+namespace ParcelHandlingSystem
+{
+    public class ParcelProcessor
+    {
+        public const string ConfigFileName = "Container_68465468.xml";
+        public Container container;
+        public static DataTable tblInsurance;
+        public static DataTable tblMail;
+        public static DataTable tblRegular;
+        public static DataTable tblHeavy;
+
+        public void ReadXML()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Container));
+            StreamReader reader = new StreamReader(Path.Combine(Common.ApplicationPath, ConfigFileName));
+
+            container = (Container)serializer.Deserialize(reader);
+            List<Parcel> allParcels = container.parcels;
+
+            var parcelUpToOneKG = from belowoneKg in allParcels
+                                  where belowoneKg.Weight <= 1 && belowoneKg.Value <= 1000
+                                  select belowoneKg;
+
+            var parcelUpToTenKG = from belowtenKg in allParcels
+                                  where belowtenKg.Weight > 1 && belowtenKg.Weight <= 10 && belowtenKg.Value <= 1000
+                                  select belowtenKg;
+
+            var parcelOverTenKG = from overtenKg in allParcels
+                                  where overtenKg.Weight > 10 && overtenKg.Value <= 1000
+                                  select overtenKg;
+
+            var parcelOverThoushand = from overThoushand in allParcels
+                                      where overThoushand.Value > 1000
+                                      select overThoushand;
+
+            IDepartment insurance = new Insurance();
+            tblInsurance = insurance.Handler(parcelOverThoushand);
+            IDepartment mail = new Mail();
+            tblMail = mail.Handler(parcelUpToOneKG);
+            IDepartment regular = new Regular();
+            tblRegular = regular.Handler(parcelUpToTenKG);
+            IDepartment heavy = new Heavy();
+            tblHeavy = heavy.Handler(parcelOverTenKG);
+        }
+    }
+}
